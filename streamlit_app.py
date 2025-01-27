ef6722257ac5e24d682d956c0adfa42b940c34d7
karinemajda <karine.majdalani@gmail.com>
2025-01-27 10:48:16 +0000

    change name of app with name of user

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈Karine Majdalani new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
